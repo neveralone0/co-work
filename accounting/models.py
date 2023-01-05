a8fa720f4db9cc036079e3bc3e5cdab408e74584
neveralone0 <aliramezani1381@gmail.com>
2023-01-05 16:25:59 +0330

    profile pic fixed

--- accounting/models.py
+++ accounting/models.py
@@ -13,7 +13,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     full_name = models.CharField(max_length=32, null=True, blank=True)
     birth_date = models.DateTimeField(null=True, blank=True)
-    picture = models.ImageField(null=True, blank=True, upload_to='profiles/%Y/%m/')
+    profile = models.ImageField(null=True, blank=True, upload_to='profiles/%Y/%m/')
     working_category = models.CharField(null=True, blank=True, choices=WORKING_CATEGORY, max_length=128, default='other')
 
     objects = managers.UserManager()
