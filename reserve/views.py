1b931f9b93ce40b10d40da1ecc456244845058c5
neveralone0 <aliramezani1381@gmail.com>
2023-01-11 18:48:28 +0330

    test2

--- reserve/views.py
+++ reserve/views.py
@@ -129,7 +129,6 @@ class ReserveDeskAPI(APIView):
         elif count >= 10:
             price -= 3 * count
 
-
         for key in data:
             date = datetime.datetime.strptime(key, "%Y-%m-%d").date()
             reservations = Desk.objects.filter(reservation__reservation_time__year=date.year,
