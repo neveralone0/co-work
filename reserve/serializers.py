0e84b2e69ff56be220b2777337f2d9bc668ca405
neveralone0 <aliramezani1381@gmail.com>
2023-01-23 23:10:38 +0330

    is passed added

--- reserve/serializers.py
+++ reserve/serializers.py
@@ -1,3 +1,6 @@
+import datetime
+
+import jalali_date
 from rest_framework import serializers
 from .models import Desk, Reservation
 from django_jalali.serializers.serializerfield import JDateField
@@ -24,10 +27,28 @@ class ReserveSerializer(serializers.ModelSerializer):
 
 class MyReserveSerializer(serializers.ModelSerializer):
     reservation_time = JDateField()
+    is_passed = serializers.SerializerMethodField('status')
+    print('+++++++++++++++')
+    print(reservation_time)
+
+    def status(self, instance):
+        print('=1')
+        now = datetime.date.today()
+        print('=2')
+        now = jalali_date.date2jalali(now).strftime('%Y-%m-%d')
+        print('=3')
+        now = datetime.datetime.strptime(now, "%Y-%m-%d").date()
+        print('========HERE==')
+        print(now)
+        # print(reservation_time)
+        if instance.reservation_time < now:
+            return True
+        else:
+            return False
 
     class Meta:
         model = Reservation
-        fields = ('reservation_time', 'order_time', 'price', 'is_group')
+        fields = ('reservation_time', 'order_time', 'price', 'is_group', 'is_passed')
     # reservation_time = serializers.DateField()
     # order_time = serializers.DateField()
     # price = serializers.IntegerField()
